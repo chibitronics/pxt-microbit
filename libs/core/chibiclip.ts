706d0728830a57c7e16cc1ba210a2fede4aa7c0e
vrk <victoriakirst@gmail.com>
2024-09-17 09:43:44 -0400

    dynamically generate svgs

--- libs/core/chibiclip.ts
+++ libs/core/chibiclip.ts
@@ -3,39 +3,76 @@
  */
 //% color=#f91b4f weight=100 icon="\uf0c6" block="Chibi Clip"
 namespace ChibiClip {
+  type DigitalPinBlockParameter = "D0" | "D1" | "D2" | "D3" | "D4" | "D5";
+
   /**
-   * Set the pin at index
+   * Turns the light on or off at the given pin value.
    */
   //% blockId=chibiclip_set
   //% block="set $pin to $on"
   //% pin.fieldEditor="textdropdown"
   //% pin.fieldOptions.decompileLiterals=true
   //% pin.fieldOptions.values='D0,D1,D2,D3,D4,D5'
-  //% pin.defl='d0'
+  //% pin.defl='D0'
   //% on.shadow="toggleOnOff"
   //% parts=chibiclip
-  export function setLight(pin: DigitalPinInput, on: boolean): void {
+  export function setLight(pin: DigitalPinBlockParameter, on: boolean): void {
     const digitalPin = stringToDigitalPin(pin);
     const value = on ? 1 : 0;
     pins.digitalWritePin(digitalPin, value);
   }
-}
 
-type DigitalPinInput = "D0"|"D1"|"D2"|"D3"|"D4"|"D5";
+  type AnalogPinBlockParameter = "A0" | "A1" | "A2" | "A3" | "A4" | "A5";
+  /**
+   * Dims the light at the pin to the given brightness.
+   */
+  //% blockId=chibiclip_setlevel
+  //% block="set $pin level to $level"
+  //% pin.fieldEditor="textdropdown"
+  //% pin.fieldOptions.decompileLiterals=true
+  //% pin.fieldOptions.values='A0,A1,A2,A3,A4,A5'
+  //% pin.defl='A0'
+  //% level.min=0 level.max=100
+  //% parts=chibiclip
+  export function setLighLevel(
+    pin: AnalogPinBlockParameter,
+    level: number
+  ): void {
+    const analogPin = stringToAnalogPin(pin);
+    pins.analogWritePin(analogPin, level);
+  }
 
-function stringToDigitalPin(pinInput : DigitalPinInput): DigitalPin {
-  switch (pinInput) {
-    case "D0":
-      return DigitalPin.P0;
-    case "D1":
-      return DigitalPin.P1;
-    case "D2":
-      return DigitalPin.P2;
-    case "D3":
-      return DigitalPin.P3;
-    case "D4":
-      return DigitalPin.P4;
-    case "D5":
-      return DigitalPin.P5;
+  function stringToDigitalPin(pinInput: DigitalPinBlockParameter): DigitalPin {
+    switch (pinInput) {
+      case "D0":
+        return DigitalPin.P0;
+      case "D1":
+        return DigitalPin.P1;
+      case "D2":
+        return DigitalPin.P2;
+      case "D3":
+        return DigitalPin.P3;
+      case "D4":
+        return DigitalPin.P4;
+      case "D5":
+        return DigitalPin.P5;
+    }
+  }
+
+  function stringToAnalogPin(pinInput: AnalogPinBlockParameter): AnalogPin {
+    switch (pinInput) {
+      case "A0":
+        return AnalogPin.P0;
+      case "A1":
+        return AnalogPin.P1;
+      case "A2":
+        return AnalogPin.P2;
+      case "A3":
+        return AnalogPin.P3;
+      case "A4":
+        return AnalogPin.P4;
+      case "A5":
+        return AnalogPin.P5;
+    }
   }
 }
--- sim/visuals/chibiclip.ts
+++ sim/visuals/chibiclip.ts
@@ -1,48 +1,131 @@
+const CLIP_WIDTH = 420;
+const CLIP_HEIGHT = 120;
+
+const NUM_PINS = 6;
+
+const ITEM_WIDTH = 30;
+const GAP = 40;
+const SPACING = ITEM_WIDTH + GAP;
+const ALL_ITEMS_WIDTH = NUM_PINS * ITEM_WIDTH + GAP * (NUM_PINS - 1);
+const X_OFFSET = (CLIP_WIDTH - ALL_ITEMS_WIDTH) / 2;
+console.log('x offset', X_OFFSET);
+
+const RECT_WIDTH = ITEM_WIDTH;
+const RECT_HEIGHT = 50;
+const RECT_Y = 70;
+const RECT_X_OFFSET = X_OFFSET;
+const RECT_X_DISTANCE = SPACING;
+const RECT_DEFAULT_FILL = 'gold';
+
+const CIRCLE_RADIUS = ITEM_WIDTH / 2;
+const CIRCLE_DEFAULT_FILL = 'gray';
+const CIRCLE_Y = 40;
+const CIRCLE_X_OFFSET = X_OFFSET + CIRCLE_RADIUS;
+const CIRCLE_X_DISTANCE = SPACING;
+
+const TEXT_Y = 140;
+const TEXT_X_OFFSET = X_OFFSET;
+const TEXT_X_DISTANCE = SPACING;
+
+
 namespace pxsim.visuals {
+  function createSvgElement(tagName: string) {
+    return document.createElementNS("http://www.w3.org/2000/svg", tagName);
+  }
+
+  function generateSvg(): SVGAElement {
+    const root = svg.parseString(`<svg xmlns="http://www.w3.org/2000/svg" width="${CLIP_WIDTH}" height="${CLIP_HEIGHT}"></svg>`);
+    const group = createSvgElement('g');
+    root.append(group);
+
+    // Add clip element
+    const clipElement = createSvgElement('rect');
+    clipElement.setAttribute('width', `${CLIP_WIDTH}`); 
+    clipElement.setAttribute('height', `${CLIP_HEIGHT}`); 
+    clipElement.setAttribute('fill', `lightblue`); 
+    group.append(clipElement);
+
+    // Add pins
+    for (let i = 0; i < NUM_PINS; i++) {
+      const pinGroup = createSvgElement('g');
+      pinGroup.setAttribute('id', `pin${i}`);
+      group.append(pinGroup);
+
+      const defaultCircle = createSvgElement('circle');
+      defaultCircle.classList.add('default');
+      defaultCircle.setAttribute('cx', `${CIRCLE_X_OFFSET + CIRCLE_X_DISTANCE * i}`);
+      defaultCircle.setAttribute('cy', `${CIRCLE_Y}`);
+      defaultCircle.setAttribute('r', `${CIRCLE_RADIUS}`);
+      defaultCircle.setAttribute('fill', CIRCLE_DEFAULT_FILL);
+      pinGroup.append(defaultCircle);
+
+      const levelCircle = createSvgElement('circle');
+      defaultCircle.classList.add('level');
+      defaultCircle.setAttribute('cx', `${CIRCLE_X_OFFSET + CIRCLE_X_DISTANCE * i}`);
+      defaultCircle.setAttribute('cy', `${CIRCLE_Y}`);
+      defaultCircle.setAttribute('r', `${CIRCLE_RADIUS}`);
+      pinGroup.append(levelCircle);
+
+      const defaultRect = createSvgElement('rect');
+      defaultRect.classList.add('default');
+      defaultRect.setAttribute('x', `${RECT_X_OFFSET + RECT_X_DISTANCE * i}`);
+      defaultRect.setAttribute('y', `${RECT_Y}`);
+      defaultRect.setAttribute('height', `${RECT_HEIGHT}`);
+      defaultRect.setAttribute('width', `${RECT_WIDTH}`);
+      defaultRect.setAttribute('fill', RECT_DEFAULT_FILL);
+      pinGroup.append(defaultRect);
+
+      const levelRect = createSvgElement('rect');
+      levelRect.classList.add('level');
+      levelRect.setAttribute('x', `${RECT_X_OFFSET + RECT_X_DISTANCE * i}`);
+      levelRect.setAttribute('y', `${RECT_Y}`);
+      levelRect.setAttribute('height', `${RECT_HEIGHT}`);
+      levelRect.setAttribute('width', `${RECT_WIDTH}`);
+      pinGroup.append(levelRect);
+
+      const labelText = createSvgElement('text');
+      labelText.classList.add('label');
+      labelText.setAttribute('x', `${TEXT_X_OFFSET + TEXT_X_DISTANCE * i}`);
+      labelText.setAttribute('y', `${TEXT_Y}`);
+      labelText.setAttribute('textAnchor', 'middle');
+      labelText.innerHTML = 'OFF';
+      pinGroup.append(labelText);
+    }
+
+    return root.firstElementChild as SVGAElement;
+  }
+
   // For the instructions parts list
   export function mkChibiClipPart(xy: Coord = [0, 0]): SVGElAndSize {
     const chibiClip = `<svg xmlns="http://www.w3.org/2000/svg" width="420" height="100">
 <g>
-  <rect width="420" height="100" rx="5" fill="lightblue"/>
+  <rect width="420" height="120" rx="5" fill="lightblue"/>
   <g id="pin0">
-      <circle cx="30" cy="50" r="10" fill="grey"/>
-      <rect width="20" height="20" x="20" y="80" fill="gold"/>
+      <circle class="default" cx="30" cy="50" r="10" fill="grey"/>
+      <circle class="level" cx="30" cy="50" r="10"/>
+      <rect class="default" width="20" height="${RECT_HEIGHT}" x="20" y="${RECT_Y}" fill="gold"/>
+      <rect class="level" width="20" height="0" x="20" y="${RECT_Y}" fill="green"/>
+      <text class="label" x="20" y="125" textAnchor="middle">ON</text>
   </g>
   <g id="pin1">
       <circle cx="70" cy="50" r="10" fill="grey"/>
-      <rect  x="60" y="80" width="20" height="20" fill="gold"/>
+      <rect x="60" y="${RECT_Y}" width="20" height=""${RECT_HEIGHT}" fill="gold"/>
   </g>
   <g id="pin2">
       <circle cx="110" cy="50" r="10" fill="grey"/>
-      <rect  x="100" y="80" width="20" height="20" fill="gold"/>
+      <rect  x="100" y="${RECT_Y}" width="20" height="${RECT_HEIGHT}" fill="gold"/>
   </g>
   <g id="pin3">
       <circle cx="150" cy="50" r="10" fill="grey"/>
-      <rect width="20" height="20" x="140" y="80" fill="gold"/>
+      <rect width="20" height="${RECT_HEIGHT}" x="140" y="${RECT_Y}" fill="gold"/>
   </g>
   <g id="pin4">
       <circle cx="190" cy="50" r="10" fill="grey"/>
-      <rect width="20" height="20" x="180" y="80" fill="gold"/>
+      <rect width="20" height="${RECT_HEIGHT}" x="180" y="${RECT_Y}" fill="gold"/>
   </g>
   <g id="pin5">
       <circle cx="230" cy="50" r="10" fill="grey"/>
-      <rect x="220" y="80" width="20" height="20" fill="gold"/>
-  </g>
-  <g id="pin6">
-      <circle cx="270" cy="50" r="10" fill="grey"/>
-      <rect x="260" y="80" width="20" height="20" fill="gold"/>
-  </g>
-  <g id="pin7">
-      <circle cx="310" cy="50" r="10" fill="grey"/>
-      <rect x="300" y="80" width="20" height="20" fill="gold"/>
-  </g>
-  <g id="pin8">
-      <circle cx="350" cy="50" r="10" fill="grey"/>
-      <rect x="340" y="80" width="20" height="20" fill="gold"/>
-  </g>
-  <g id="pin9">
-      <circle cx="390" cy="50" r="10" fill="grey"/>
-      <rect x="380" y="80" width="20" height="20" fill="gold"/>
+      <rect x="220" y="${RECT_Y}" width="20" height="${RECT_HEIGHT}" fill="gold"/>
   </g>
 </g>
 </svg>`;
@@ -51,7 +134,8 @@ namespace pxsim.visuals {
     let t = y;
     let w = 420;
     let h = 100;
-    const el = svg.parseString(chibiClip).firstElementChild as SVGGElement;
+    // const el = svg.parseString(chibiClip).firstElementChild as SVGGElement;
+    const el = generateSvg();
     return { el, x: l, y: t, w: w, h: h };
   }
   export class ChibiClipView implements IBoardPart<EdgeConnectorState> {
@@ -102,20 +186,65 @@ namespace pxsim.visuals {
 
     public updateState(): void {
       for (let i = 0; i < 10; i++) {
-        const pinEl = this.element.querySelector(`#pin${i} rect`);
-        const pinLedEl = this.element.querySelector(`#pin${i} circle`);
-        if (!pinEl || !pinLedEl) {
-          continue;
+        const pinLoaded = this.element.querySelector(`#pin${i}`);
+        if (!pinLoaded) {
+          return;
         }
-        if (this.state.pins[i].value > 1000) {
-          pinEl.setAttribute("fill", "red");
-          pinLedEl.setAttribute("fill", "yellow");
+        const pin = this.state.pins[i];
+
+        const isAnalog = (pin.mode & PinFlags.Analog) !== 0;
+        const isDigital = (pin.mode & PinFlags.Digital) !== 0;
+        if (isAnalog) {
+          this.setAnalogDisplay(i);
+        } else if (isDigital) {
+          this.setDigitalDisplay(i);
         } else {
-          pinEl.setAttribute("fill", "gold");
-          pinLedEl.setAttribute("fill", "gray");
+          this.resetPin(i);
         }
       }
     }
+
+    private resetPin(index: number) {
+      const pinFillEl = this.element.querySelector(`#pin${index} rect.level`);
+      const pinLedFillEl = this.element.querySelector(`#pin${index} circle.level`);
+      const pinLabelEl = this.element.querySelector(`#pin${index} text.label`);
+
+      // pinLedFillEl.setAttribute("fill", "");
+      // pinFillEl.setAttribute("height", "0")
+      // pinFillEl.setAttribute("y", `${RECT_Y}`);
+      // pinLabelEl.innerHTML = "";
+    }
+
+    private setDigitalDisplay(index: number) {
+      const pin = this.state.pins[index];
+      U.assert((pin.mode & PinFlags.Digital) !== 0);
+      const value = pin.value > 0 ? "0%" : "100%";
+
+      const pinFillEl = this.element.querySelector(`#pin${index} rect.level`);
+      const pinLedFillEl = this.element.querySelector(`#pin${index} circle.level`);
+      const pinLabelEl = this.element.querySelector(`#pin${index} text.label`);
+
+      pinFillEl.setAttribute("fill", "green");
+      pinFillEl.setAttribute("height", `${RECT_HEIGHT}`);
+      pinFillEl.setAttribute("y", `${RECT_Y - RECT_HEIGHT}`);
+
+      pinLedFillEl.setAttribute("fill", "red");
+      
+      pinLabelEl.innerHTML = value;
+    }
+
+    private setAnalogDisplay(index: number) {
+      const pin = this.state.pins[index];
+      U.assert((pin.mode & PinFlags.Analog) !== 0);
+      const value = Math.round(100 - (pin.value || 0) / 255 * 100) + "%";
+
+      const pinFillEl = this.element.querySelector(`#pin${index} rect.level`);
+      const pinLedFillEl = this.element.querySelector(`#pin${index} circle.level`);
+      const pinLabelEl = this.element.querySelector(`#pin${index} text.label`);
+      
+      pinLabelEl.innerHTML = value;
+    }
+
     public updateTheme(): void {}
   }
 }
